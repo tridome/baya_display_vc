87e6a643098cc088fcd90916e1219a017d4a714a
tridome <tech@tridomestructures.com>
2025-05-13 11:28:40 -0400

    Files

---
+++ data.js
@@ -0,0 +1,101 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-panorama3",
+      "name": "Panorama(3)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.0051037644394149595,
+          "pitch": 0.0410063977776538,
+          "rotation": 0,
+          "target": "1-scene-91"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.46598861236452827,
+          "pitch": 0.19275419998891152,
+          "title": "Good question",
+          "text": "More questions?"
+        }
+      ]
+    },
+    {
+      "id": "1-scene-91",
+      "name": "Scene 9(1)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 2.5894500913730436,
+        "pitch": 0.18470382398880147,
+        "fov": 1.3555154520896788
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.829626262649466,
+          "pitch": 0.060219344963856614,
+          "rotation": 0,
+          "target": "0-panorama3"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.9680695419168366,
+          "pitch": -0.05965844289494626,
+          "title": "This is a Test",
+          "text": "Here is your text:&nbsp;https://placehold.co/"
+        }
+      ]
+    }
+  ],
+  "name": "EJH_Test-R01",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
